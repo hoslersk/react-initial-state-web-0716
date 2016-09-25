06a246df8faac7a0c4122e522421187322cabca1
Schyler Hosler <schylerhosler@gmail.com>
2016-09-25 15:22:59 -0400

    Initial commit: React.js lesson/intro to states vs props in dynamic rendering

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much.";
+// const app = "I don't do much.";
+
+class ToggleButton extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isEnabled: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="toggle-button">I am toggled {this.state.isEnabled ? 'on' : 'off'}.</div>
+    );
+  }
+}
+
+
+
+// class Address extends React.Component {
+//   constructor(props) {
+//     super();
+//
+//     this.state = {
+//       fullAddress: `${props.street}, ${props.city}`
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <div className="address">{this.state.fullAddress}</div> // Don't do this
+//     );
+//   }
+// }
+
+// class Address extends React.Component {
+//   render() {
+//     return (
+//       <div className="address">{this.props.street}, {this.props.city}</div> // Do this
+//     );
+//   }
+// }
